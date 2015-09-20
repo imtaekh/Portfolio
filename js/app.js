310d8c35363653e8a034953ef47b2d907e4651c8
Imtaek Hong <imtaekh@gmail.com>
2015-09-20 16:44:03 -0700

    init() delay updated

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@
           $scope.$apply(function () {
             $scope.mainHeight={maxHeight:"400px", maxWidth:'700px',transition: "max-height, 3s"};
           });
-        },1000);
+        }, 200);
         setTimeout(function () {
           $scope.$apply(function () {
             for (var i = 0; i < $scope.menuItem.length; i++) {
